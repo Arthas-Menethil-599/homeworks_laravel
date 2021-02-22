daacff6c663e5f2bf8deb70c391d18ff466d2b34
David <sir.baimyrza@gmail.com>
2021-02-22 17:36:51 +0600

    test commit

---
+++ homework/database/migrations/2021_02_22_104915_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            //Решил что имя продукта не будет уникальным так как бывают одни и те же продукты
+            //от разных компаний или пользователей выставляющих товар
+            $table->string('product_name');
+            $table->decimal('product_price',8,2);
+            $table->string('product_description');
+            $table->string('product_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
